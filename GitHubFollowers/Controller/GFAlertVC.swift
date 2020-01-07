99a652533c7434a46461d59bfdf7e82695fca939
Darragh <darragh.blake@urbanairship.com>
2020-01-07 22:02:10 +0000

    Creating Reusable Customer Alter 1/2

---
+++ GitHubFollowers/Controller/GFAlertVC.swift
@@ -0,0 +1,38 @@
+//
+//  GFAlertVC.swift
+//  GitHubFollowers
+//
+//  Created by Darragh Blake on 07/01/2020.
+//  Copyright © 2020 Darragh Blake. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertVC: UIViewController {
+    
+    let containerView = GFContainerView()
+    let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
+    let messageLabel = GFBodyLabel(textAlignment: .center)
+    let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
+    
+    var alertTitle: String?
+    var message: String?
+    var buttonTitle: String?
+    
+    init(title: String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle = title
+        self.message = message
+        self.buttonTitle = buttonTitle
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ GitHubFollowers/Utils/GFContainerView.swift
@@ -0,0 +1,26 @@
+//
+//  GFContainerView.swift
+//  GitHubFollowers
+//
+//  Created by Darragh Blake on 07/01/2020.
+//  Copyright © 2020 Darragh Blake. All rights reserved.
+//
+
+import UIKit
+
+class GFContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        
+    }
+    
+}
