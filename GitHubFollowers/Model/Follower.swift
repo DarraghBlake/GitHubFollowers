8227d9ea9b4c7fbdbe8f9e475e31eb65e98f2647
Darragh <darragh.blake@urbanairship.com>
2020-01-09 19:41:40 +0000

    Added Model Follower & User

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Darragh Blake on 09/01/2020.
+//  Copyright © 2020 Darragh Blake. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    let login: String
+    let avatarUrl: String
+}
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by Darragh Blake on 09/01/2020.
+//  Copyright © 2020 Darragh Blake. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let login: String
+    let avatarUrl: String
+    let name: String?
+    let location: String?
+    let bio: String?
+    let publicRepos: Int
+    let publicGists: Int
+    let followers: Int
+    let following: Int
+    let htmlUrl: String
+    let createdAt: String
+}
