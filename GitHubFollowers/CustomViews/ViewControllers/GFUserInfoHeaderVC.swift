9a067580aee0daf6a617aecd9b8ec82c060fcb93
Darragh <darragh.blake@urbanairship.com>
2020-01-23 18:44:54 +0000

    Configured GFSecondaryTitleLabel

---
+++ GitHubFollowers/CustomViews/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,18 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GitHubFollowers
+//
+//  Created by Darragh Blake on 23/01/2020.
+//  Copyright © 2020 Darragh Blake. All rights reserved.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
