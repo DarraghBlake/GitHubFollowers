cacd57507580a82d040b8857ff4edb5717de18c1
Darragh <darragh.blake@urbanairship.com>
2020-01-25 21:03:15 +0000

    Add Button Tapped on UserInfoVC

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -43,6 +43,9 @@ class FollowerListVC: UIViewController {
     func configureViewControlloer() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
     }
     
     func configureCollectionView() {
@@ -96,6 +99,10 @@ class FollowerListVC: UIViewController {
         snapShot.appendSections([.main])
         snapShot.appendItems(followers)
         DispatchQueue.main.async { self.dataSource.apply(snapShot, animatingDifferences: true) }
+    }
+    
+    @objc func addButtonTapped() {
+        print("Add button tapped")
     }
 }
 
