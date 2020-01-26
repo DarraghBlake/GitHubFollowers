4f4f04ffd9a4b76d237f910a8a48e6a78456ef8e
Darragh <darragh.blake@urbanairship.com>
2020-01-26 15:08:14 +0000

    Persistence Manager - Logic Implementation

--- GitHubFollowers/Managers/PersistenceManager.swift
+++ GitHubFollowers/Managers/PersistenceManager.swift
@@ -37,7 +37,7 @@ enum PersistenceManager {
                     retrievedFavorites.removeAll { $0.login == favorite.login }
                 }
                 
-                completed(save(favorites: favorites))
+                completed(save(favorites: retrievedFavorites))
                 
             case .failure(let error):
                 completed(error)
--- GitHubFollowers/Screens/FavouritesListVC.swift
+++ GitHubFollowers/Screens/FavouritesListVC.swift
@@ -13,6 +13,15 @@ class FavouritesListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
 
 }
